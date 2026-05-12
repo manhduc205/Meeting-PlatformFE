27bf38757e3df377dab07502a499a759cba5382c
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2026-05-12 11:04:42 +0700

    docs: add environment configuration and comprehensive API documentation</code>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 export const environment = {
   production: true,
   keycloak: {
-    url: 'http://localhost:8080',
+    url: 'https://ducdev.duckdns.org', 
     realm: 'meeting-realm',
     clientId: 'meeting-client'
   },
-  backendApiUrl: 'http://localhost:8081',
-  // Get it from: https://console.cloud.google.com/apis/credentials
+  backendApiUrl: 'https://ducdev.duckdns.org', 
   googleClientId: '31196052853-qokps763iu3glinq4sviht7q58n87bv7.apps.googleusercontent.com'
 };
