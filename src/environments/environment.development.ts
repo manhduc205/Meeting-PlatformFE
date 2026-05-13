ef7cb2cd604e1749d568f3601e37aeab4e34f0fe
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2026-05-14 00:29:23 +0700

    feat: implement environment configuration and update API documentation

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   keycloak: {
-    url: 'https://ducdev.duckdns.org',
+    url: 'https://devduc.duckdns.org',
     realm: 'meeting-realm',
     clientId: 'meeting-client'
   },
-  backendApiUrl: 'https://ducdev.duckdns.org',
+  backendApiUrl: 'https://devduc.duckdns.org',
   googleClientId: '31196052853-qokps763iu3glinq4sviht7q58n87bv7.apps.googleusercontent.com'
 };
