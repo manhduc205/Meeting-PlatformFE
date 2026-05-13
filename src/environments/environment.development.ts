806b5e68b4f44546740110049c0947d335be3e08
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2026-05-13 13:36:44 +0700

    chore: initialize environment configuration and restructure API documentation with Vietnamese localization

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   keycloak: {
-    url: 'https://ducdev.duckdns.org', 
+    url: 'https://ducdev.duckdns.org',
     realm: 'meeting-realm',
     clientId: 'meeting-client'
   },
-  backendApiUrl: 'https://ducdev.duckdns.org', 
+  backendApiUrl: 'https://ducdev.duckdns.org',
   googleClientId: '31196052853-qokps763iu3glinq4sviht7q58n87bv7.apps.googleusercontent.com'
 };
